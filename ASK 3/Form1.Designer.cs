df344ff73651544b508ec6246cbc155181df3e40
Jan Mikicki <jan.mikicki@outlook.com>
2019-04-07 16:33:16 +0200

    pierwszy dzien

--- ASK 3/Form1.Designer.cs
+++ ASK 3/Form1.Designer.cs
@@ -28,13 +28,376 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.buttonDot = new System.Windows.Forms.Button();
+            this.button0 = new System.Windows.Forms.Button();
+            this.buttonDivide = new System.Windows.Forms.Button();
+            this.buttonMultiply = new System.Windows.Forms.Button();
+            this.buttonSubstract = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.tbDisplay = new System.Windows.Forms.TextBox();
+            this.pictureBoxAnalog = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.skinyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zegarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.analogowyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cyfrowyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.labelDigital = new System.Windows.Forms.Label();
+            this.defaultToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.blackToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buttonClearEntry = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonEquals = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAnalog)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(39, 241);
+            this.button1.Margin = new System.Windows.Forms.Padding(6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(101, 241);
+            this.button2.Margin = new System.Windows.Forms.Padding(6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 45);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(163, 241);
+            this.button3.Margin = new System.Windows.Forms.Padding(6);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(50, 45);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(39, 184);
+            this.button4.Margin = new System.Windows.Forms.Padding(6);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(50, 45);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(101, 184);
+            this.button5.Margin = new System.Windows.Forms.Padding(6);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(50, 45);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(163, 184);
+            this.button6.Margin = new System.Windows.Forms.Padding(6);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(50, 45);
+            this.button6.TabIndex = 3;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(39, 127);
+            this.button7.Margin = new System.Windows.Forms.Padding(6);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(50, 45);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(101, 127);
+            this.button8.Margin = new System.Windows.Forms.Padding(6);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(50, 45);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(163, 127);
+            this.button9.Margin = new System.Windows.Forms.Padding(6);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(50, 45);
+            this.button9.TabIndex = 6;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // buttonDot
+            // 
+            this.buttonDot.Location = new System.Drawing.Point(163, 298);
+            this.buttonDot.Margin = new System.Windows.Forms.Padding(6);
+            this.buttonDot.Name = "buttonDot";
+            this.buttonDot.Size = new System.Drawing.Size(50, 45);
+            this.buttonDot.TabIndex = 11;
+            this.buttonDot.Text = ".";
+            this.buttonDot.UseVisualStyleBackColor = true;
+            // 
+            // button0
+            // 
+            this.button0.Location = new System.Drawing.Point(39, 298);
+            this.button0.Margin = new System.Windows.Forms.Padding(6);
+            this.button0.Name = "button0";
+            this.button0.Size = new System.Drawing.Size(112, 45);
+            this.button0.TabIndex = 9;
+            this.button0.Text = "0";
+            this.button0.UseVisualStyleBackColor = true;
+            // 
+            // buttonDivide
+            // 
+            this.buttonDivide.Location = new System.Drawing.Point(225, 127);
+            this.buttonDivide.Margin = new System.Windows.Forms.Padding(6);
+            this.buttonDivide.Name = "buttonDivide";
+            this.buttonDivide.Size = new System.Drawing.Size(50, 45);
+            this.buttonDivide.TabIndex = 14;
+            this.buttonDivide.Text = "/";
+            this.buttonDivide.UseVisualStyleBackColor = true;
+            // 
+            // buttonMultiply
+            // 
+            this.buttonMultiply.Location = new System.Drawing.Point(225, 184);
+            this.buttonMultiply.Margin = new System.Windows.Forms.Padding(6);
+            this.buttonMultiply.Name = "buttonMultiply";
+            this.buttonMultiply.Size = new System.Drawing.Size(50, 45);
+            this.buttonMultiply.TabIndex = 13;
+            this.buttonMultiply.Text = "*";
+            this.buttonMultiply.UseVisualStyleBackColor = true;
+            // 
+            // buttonSubstract
+            // 
+            this.buttonSubstract.Location = new System.Drawing.Point(225, 241);
+            this.buttonSubstract.Margin = new System.Windows.Forms.Padding(6);
+            this.buttonSubstract.Name = "buttonSubstract";
+            this.buttonSubstract.Size = new System.Drawing.Size(50, 45);
+            this.buttonSubstract.TabIndex = 12;
+            this.buttonSubstract.Text = "-";
+            this.buttonSubstract.UseVisualStyleBackColor = true;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(225, 298);
+            this.buttonAdd.Margin = new System.Windows.Forms.Padding(6);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(50, 45);
+            this.buttonAdd.TabIndex = 15;
+            this.buttonAdd.Text = "+";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            // 
+            // tbDisplay
+            // 
+            this.tbDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbDisplay.Location = new System.Drawing.Point(39, 59);
+            this.tbDisplay.Name = "tbDisplay";
+            this.tbDisplay.Size = new System.Drawing.Size(298, 40);
+            this.tbDisplay.TabIndex = 16;
+            this.tbDisplay.Text = "0";
+            this.tbDisplay.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // pictureBoxAnalog
+            // 
+            this.pictureBoxAnalog.Location = new System.Drawing.Point(395, 59);
+            this.pictureBoxAnalog.Name = "pictureBoxAnalog";
+            this.pictureBoxAnalog.Size = new System.Drawing.Size(170, 170);
+            this.pictureBoxAnalog.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxAnalog.TabIndex = 20;
+            this.pictureBoxAnalog.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.skinyToolStripMenuItem,
+            this.zegarToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(621, 24);
+            this.menuStrip1.TabIndex = 21;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // skinyToolStripMenuItem
+            // 
+            this.skinyToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.defaultToolStripMenuItem,
+            this.blackToolStripMenuItem});
+            this.skinyToolStripMenuItem.Name = "skinyToolStripMenuItem";
+            this.skinyToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.skinyToolStripMenuItem.Text = "skiny";
+            // 
+            // zegarToolStripMenuItem
+            // 
+            this.zegarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.analogowyToolStripMenuItem,
+            this.cyfrowyToolStripMenuItem});
+            this.zegarToolStripMenuItem.Name = "zegarToolStripMenuItem";
+            this.zegarToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.zegarToolStripMenuItem.Text = "Zegar";
+            // 
+            // analogowyToolStripMenuItem
+            // 
+            this.analogowyToolStripMenuItem.Name = "analogowyToolStripMenuItem";
+            this.analogowyToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.analogowyToolStripMenuItem.Text = "Analogowy";
+            this.analogowyToolStripMenuItem.Click += new System.EventHandler(this.analogowyToolStripMenuItem_Click);
+            // 
+            // cyfrowyToolStripMenuItem
+            // 
+            this.cyfrowyToolStripMenuItem.Name = "cyfrowyToolStripMenuItem";
+            this.cyfrowyToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.cyfrowyToolStripMenuItem.Text = "Cyfrowy";
+            this.cyfrowyToolStripMenuItem.Click += new System.EventHandler(this.cyfrowyToolStripMenuItem_Click);
+            // 
+            // labelDigital
+            // 
+            this.labelDigital.AutoSize = true;
+            this.labelDigital.Font = new System.Drawing.Font("MS Reference Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDigital.Location = new System.Drawing.Point(376, 59);
+            this.labelDigital.Name = "labelDigital";
+            this.labelDigital.Size = new System.Drawing.Size(224, 40);
+            this.labelDigital.TabIndex = 22;
+            this.labelDigital.Text = "00:00:00 PM";
+            this.labelDigital.Visible = false;
+            // 
+            // defaultToolStripMenuItem
+            // 
+            this.defaultToolStripMenuItem.Name = "defaultToolStripMenuItem";
+            this.defaultToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.defaultToolStripMenuItem.Text = "default";
+            this.defaultToolStripMenuItem.Click += new System.EventHandler(this.defaultToolStripMenuItem_Click);
+            // 
+            // blackToolStripMenuItem
+            // 
+            this.blackToolStripMenuItem.Name = "blackToolStripMenuItem";
+            this.blackToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.blackToolStripMenuItem.Text = "black";
+            this.blackToolStripMenuItem.Click += new System.EventHandler(this.blackToolStripMenuItem_Click);
+            // 
+            // buttonClearEntry
+            // 
+            this.buttonClearEntry.Location = new System.Drawing.Point(287, 127);
+            this.buttonClearEntry.Margin = new System.Windows.Forms.Padding(6);
+            this.buttonClearEntry.Name = "buttonClearEntry";
+            this.buttonClearEntry.Size = new System.Drawing.Size(50, 45);
+            this.buttonClearEntry.TabIndex = 17;
+            this.buttonClearEntry.Text = "CE";
+            this.buttonClearEntry.UseVisualStyleBackColor = true;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(287, 184);
+            this.buttonClear.Margin = new System.Windows.Forms.Padding(6);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(50, 45);
+            this.buttonClear.TabIndex = 18;
+            this.buttonClear.Text = "C";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            // 
+            // buttonEquals
+            // 
+            this.buttonEquals.Location = new System.Drawing.Point(287, 241);
+            this.buttonEquals.Margin = new System.Windows.Forms.Padding(6);
+            this.buttonEquals.Name = "buttonEquals";
+            this.buttonEquals.Size = new System.Drawing.Size(50, 102);
+            this.buttonEquals.TabIndex = 19;
+            this.buttonEquals.Text = "=";
+            this.buttonEquals.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(621, 405);
+            this.Controls.Add(this.labelDigital);
+            this.Controls.Add(this.pictureBoxAnalog);
+            this.Controls.Add(this.buttonEquals);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonClearEntry);
+            this.Controls.Add(this.tbDisplay);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.buttonDivide);
+            this.Controls.Add(this.buttonMultiply);
+            this.Controls.Add(this.buttonSubstract);
+            this.Controls.Add(this.buttonDot);
+            this.Controls.Add(this.button0);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAnalog)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button buttonDot;
+        private System.Windows.Forms.Button button0;
+        private System.Windows.Forms.Button buttonDivide;
+        private System.Windows.Forms.Button buttonMultiply;
+        private System.Windows.Forms.Button buttonSubstract;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.TextBox tbDisplay;
+        private System.Windows.Forms.PictureBox pictureBoxAnalog;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem skinyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zegarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem analogowyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cyfrowyToolStripMenuItem;
+        private System.Windows.Forms.Label labelDigital;
+        private System.Windows.Forms.ToolStripMenuItem defaultToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem blackToolStripMenuItem;
+        private System.Windows.Forms.Button buttonClearEntry;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonEquals;
     }
 }
 
--- ASK 3/Form1.cs
+++ ASK 3/Form1.cs
@@ -12,9 +12,152 @@ namespace ASK_3
 {
     public partial class Form1 : Form
     {
+        Timer t = new Timer();
+        int WIDTH = 200, HEIGHT = 200, secHAND = 90, minHAND = 80, hrHAND = 50;
+
+        //center
+        int cx, cy;
+
+        Bitmap bmp;
+        Graphics g;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void cyfrowyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.pictureBoxAnalog.Hide();
+            this.labelDigital.Show();
+        }
+
+        private void analogowyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.pictureBoxAnalog.Show();
+            this.labelDigital.Hide();
+        }
+
+        private void blackToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Black;
+            this.labelDigital.ForeColor = Color.WhiteSmoke;
+           
+        }
+
+        private void defaultToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.AntiqueWhite;
+            this.labelDigital.ForeColor = Color.Black;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //create bitmap
+            bmp = new Bitmap(WIDTH + 1, HEIGHT + 1);
+
+            //center
+            cx = WIDTH / 2;
+            cy = HEIGHT / 2;
+
+            //backcolor
+            this.BackColor = Color.White;
+
+            //timer
+            t.Interval = 1000;      //in millisecond
+            t.Tick += new EventHandler(this.t_Tick);
+            t.Start();
+        }
+
+        private void t_Tick(object sender, EventArgs e)
+        {
+            //create graphics
+            g = Graphics.FromImage(bmp);
+
+            //get time
+            int ss = DateTime.Now.Second;
+            int mm = DateTime.Now.Minute;
+            int hh = DateTime.Now.Hour;
+
+            int[] handCoord = new int[2];
+
+            //clear
+            g.Clear(Color.White);
+
+            //draw circle
+            g.DrawEllipse(new Pen(Color.Black, 1f), 0, 0, WIDTH, HEIGHT);
+
+            //draw figure
+            g.DrawString("12", new Font("Arial", 12), Brushes.Black, new PointF(90, 2));
+            g.DrawString("3", new Font("Arial", 12), Brushes.Black, new PointF(182, 90));
+            g.DrawString("6", new Font("Arial", 12), Brushes.Black, new PointF(92, 182));
+            g.DrawString("9", new Font("Arial", 12), Brushes.Black, new PointF(0, 90));
+
+            //second hand
+            handCoord = msCoord(ss, secHAND);
+            g.DrawLine(new Pen(Color.Red, 1f), new Point(cx, cy), new Point(handCoord[0], handCoord[1]));
+
+            //minute hand
+            handCoord = msCoord(mm, minHAND);
+            g.DrawLine(new Pen(Color.Black, 2f), new Point(cx, cy), new Point(handCoord[0], handCoord[1]));
+
+            //hour hand
+            handCoord = hrCoord(hh % 12, mm, hrHAND);
+            g.DrawLine(new Pen(Color.Gray, 3f), new Point(cx, cy), new Point(handCoord[0], handCoord[1]));
+
+            //load bmp in picturebox1
+            pictureBoxAnalog.Image = bmp;
+
+            //disp time
+            this.Text = "Analog Clock -  " + hh + ":" + mm + ":" + ss;
+
+            this.labelDigital.Text = DateTime.Now.ToString("T");
+            //dispose
+            g.Dispose();
+        }
+
+        private int[] msCoord(int val, int hlen)
+        {
+            int[] coord = new int[2];
+            val *= 6;   //each minute and second make 6 degree
+
+            if (val >= 0 && val <= 180)
+            {
+                coord[0] = cx + (int)(hlen * Math.Sin(Math.PI * val / 180));
+                coord[1] = cy - (int)(hlen * Math.Cos(Math.PI * val / 180));
+            }
+            else
+            {
+                coord[0] = cx - (int)(hlen * -Math.Sin(Math.PI * val / 180));
+                coord[1] = cy - (int)(hlen * Math.Cos(Math.PI * val / 180));
+            }
+            return coord;
+        }
+        
+        private int[] hrCoord(int hval, int mval, int hlen)
+        {
+            int[] coord = new int[2];
+
+            //each hour makes 30 degree
+            //each min makes 0.5 degree
+            int val = (int)((hval * 30) + (mval * 0.5));
+
+            if (val >= 0 && val <= 180)
+            {
+                coord[0] = cx + (int)(hlen * Math.Sin(Math.PI * val / 180));
+                coord[1] = cy - (int)(hlen * Math.Cos(Math.PI * val / 180));
+            }
+            else
+            {
+                coord[0] = cx - (int)(hlen * -Math.Sin(Math.PI * val / 180));
+                coord[1] = cy - (int)(hlen * Math.Cos(Math.PI * val / 180));
+            }
+            return coord;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
